47a21534e3a916ffa6898852b7229a2270c3262b
SalemMohamed100 <124922909+SalemMohamed100@users.noreply.github.com>
2024-02-29 00:23:23 +0200

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,16 +1,20 @@
+import gigRoute from "./routes/gig.route.js";
+import orderRoute from "./routes/order.route.js";
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoute from "./routes/user.route.js";
-import gigRoute from "./routes/gig.route.js";
-import orderRoute from "./routes/order.route.js";
+import mongoose from "mongoose";
 import conversationRoute from "./routes/conversation.route.js";
 import messageRoute from "./routes/message.route.js";
-import reviewRoute from "./routes/review.route.js";
+import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+
+import reviewRoute from "./routes/review.route.js";
 const app = express();
 dotenv.config();
 mongoose.set("strictQuery", true);
